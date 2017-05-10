86749c7f14389c384beb0efa797ea40372d07306
JohanNyan <johanadam95@gmail.com>
2017-05-10 20:36:10 +0800

    Add nav bar ; before remove all about favorite and make list

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 #separated html from python
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 # Create your views here.
 #request is html request
@@ -11,3 +11,29 @@ def index(request):
 def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        #try get something
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        #can't get it
+        return render(request, 'music/detail.html', {
+            'album':album,
+            'error_message':"You did not select a valid sond",
+        })
+    else:
+        #after get try get everything it wants
+        if selected_song.is_favourite:
+            selected_song.is_favourite = False
+        else:
+            selected_song.is_favourite = True
+
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
+
+
+
+

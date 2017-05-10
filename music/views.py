6ab0a78c35eab4c1e5ebb9cc1abc86b78ce5be3c
JohanNyan <johanadam95@gmail.com>
2017-05-10 21:44:12 +0800

    Refactor code , use generic view to short and clean code

--- music/views.py
+++ music/views.py
@@ -1,39 +1,23 @@
-#separated html from python
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
-
-# Create your views here.
-#request is html request
-def index(request):
-    all_albumpara = Album.objects.all()
-    return render(request, 'music/index.html', {'all_albumss': all_albumpara})
-
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album})
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        #try get something
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        #can't get it
-        return render(request, 'music/detail.html', {
-            'album':album,
-            'error_message':"You did not select a valid sond",
-        })
-    else:
-        #after get try get everything it wants
-        if selected_song.is_favourite:
-            selected_song.is_favourite = False
-        else:
-            selected_song.is_favourite = True
-
-        selected_song.save()
-        return render(request, 'music/detail.html', {'album': album})
+# separated html from python
+from django.views import generic
+from .models import Album
 
 
+# List generic view
+class IndexView(generic.ListView):
+    # get the view
+    template_name = 'music/index.html'
+    # give parameter to list return
+    context_object_name = "all_albums"
+
+    # the data this view need
+    def get_queryset(self):
+        return Album.objects.all()
+        # by default it return as object_list to the layout
 
 
-
+# List generic view
+class DetailView(generic.DetailView):
+    model = Album
+    # get the view
+    template_name = 'music/detail.html'
